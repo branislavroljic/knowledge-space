754fea39daa35b243668cff33af769d41af0eb29
Branislav Roljic <branislavroljic99@gmail.com>
2023-12-06 00:07:25 +0100

    Initial commit. added fe app for students

---
+++ knowledge_space_project/src/main/java/com/example/controller/AssessmentTestController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/assessment_tests`")
+@RequiredArgsConstructor
+public class AssessmentTestController {}
---
+++ knowledge_space_project/src/main/java/com/example/model/dto/AssessmentTest.java
@@ -0,0 +1,11 @@
+package com.example.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class AssessmentTest {
+  private Integer id;
+  private String name;
+}
